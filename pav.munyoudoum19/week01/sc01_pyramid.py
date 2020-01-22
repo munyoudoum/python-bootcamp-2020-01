c01733dc3d7be17728f65bc8b8544e58b81155bb
munyoudoum <pav.munyoudoum19@kit.edu.kh>
2020-01-22 14:19:53 +0700

    Done bonus challenge

---
+++ pav.munyoudoum19/week01/sc01_pyramid.py
@@ -0,0 +1,19 @@
+def pyramid(level, symbol, is_reverse=False):
+    if level <= 1:
+        print("Invalid parameters")
+    else:
+        if is_reverse == False:
+            space = level-1
+            num=1
+            while space >= 0:
+                print(" "*space+symbol*num)
+                num+=2
+                space-=1
+        else:
+            space=0
+            num = (2 * level-1)
+            while space < level:
+                print(" "*space+symbol*num)
+                num-=2
+                space+=1
+pyramid(3,"A", is_reverse=True)
